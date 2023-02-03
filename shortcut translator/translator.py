e3d424244951489e5bd6eebf88c9405fab94ccdf
unknown <edjjxx1@gmail.com>
2023-02-04 00:20:50 +0800

    added rest api request to keeptranslations

--- shortcut translator/translator.py
+++ shortcut translator/translator.py
@@ -1,25 +1,27 @@
-import pyautogui
 import win32api
 import wx
 import wx.adv
-# import ctypes
 import time
 import pyperclip
 import keyboard
-# import multiprocessing
 import threading
+import requests
 
 from sql import DataBase
-# import msvcrt
-from google_translate import make_google_translation
 
 TRAY_ICON = 'united-kingdom (1).png'
-TIME_SLEEP_BETWEEN_KEYPRESS = 0.3
+TIME_SLEEP_BETWEEN_KEYPRESS = 0.2
+EMPTY_NAME = "<<error>>"
+API_URL = "http://127.0.0.1:8000/api/v1/translate/"
+
+LETTERS_RUS = "фисвуапршолдьтщзйкыегмцчняФИСDEÀÏРШJËДЬТЩЗЙКЫЕГМЦЧНЯ"
+LETTERS_ENG = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 db = DataBase("s_translator.db")
 db.connect()
 db.create_table_if_not_exists()
 
+last_word = {'word': None, 'translation': None}
 
 
 def create_menu_item(menu, label, func):
@@ -87,23 +89,24 @@ class PopupWindow(wx.Frame):
             message_list = [message_list[0].strip()]
 
         self.st = []
+        text_width, text_height = 0, 0
         for i in range(0, len(message_list)):
             st_t = wx.StaticText(self, label=message_list[i], pos=(0, 0))
             st_t.SetFont(font)
             self.st.append(st_t)
-        raw_max_length_index = \
-            [x for x in range(0, len(message_list)) if len(message_list[x]) == max([len(x) for x in message_list])][0]
-        # print(f"{raw_max_length_index =}")
-
-        dc = wx.ClientDC(self.st[raw_max_length_index])
-        text_width, text_height = dc.GetTextExtent(self.st[raw_max_length_index].GetLabel())
+            dc = wx.ClientDC(self.st[i])
+            text_dimensions = dc.GetTextExtent(self.st[i].GetLabel())
+            if text_dimensions[0] > text_width:
+                text_width = text_dimensions[0]
+            if text_dimensions[1] > text_height:
+                text_height = text_dimensions[1]
 
         screen_size = wx.DisplaySize()
         # print(f"{pos = }")
         # print(f"{screen_size = }")
         if text_width + 10 * 2 + pos[0] > screen_size[0]:
             pos = (
-            screen_size[0] - (text_width + 10 * 2) - 50, mouse_pos[1] - (text_height * len(message_list) + 20) - 20)
+                screen_size[0] - (text_width + 10 * 2) - 50, mouse_pos[1] - (text_height * len(message_list) + 20) - 20)
         else:
             pos = (mouse_pos[0] + 20, mouse_pos[1] - (text_height * len(message_list) + 20) - 20)
         self.SetPosition(pos)
@@ -112,7 +115,7 @@ class PopupWindow(wx.Frame):
         for i in range(0, len(self.st)):
             self.st[i].SetPosition((10, i * text_height + 10 - 2))
         self.Bind(wx.EVT_LEFT_DOWN, self.on_key_pressed, id=wx.ID_ANY)
-        self.st[0].Bind(wx.EVT_MOTION, self.on_mouse_moved, id=wx.ID_ANY)
+        self.Bind(wx.EVT_MOTION, self.on_mouse_moved, id=wx.ID_ANY)
 
     def on_mouse_moved(self, event):
         print("mouse moved")
@@ -121,8 +124,6 @@ class PopupWindow(wx.Frame):
     def on_key_pressed(self, event):
         print("Key pressed")
         self.Close()
-        # a = wx.TipWindow(self, "This is a tooltip", 500)
-        # a.show()
 
     def on_timer(self, event):
         self.Close()
@@ -141,138 +142,138 @@ class MainWindow(wx.Frame):
 
         self.timer = wx.Timer(self)
 
-        # self.Bind(wx.EVT_LEFT_DOWN, self.on_key_pressed, id=wx.ID_ANY)
-        self.Bind(wx.EVT_ICONIZE, self.OnMyEvent, id=10)
-        self.Bind(wx.EVT_ICONIZE, self.on_translit, id=20)
+        self.Bind(wx.EVT_ICONIZE, self.new_frame, id=10)
+        self.Bind(wx.EVT_ICONIZE, self.do_translit, id=20)
         self.Bind(wx.EVT_TIMER, self.on_timer, self.timer)
 
     def on_timer(self, event):
-        self.Close_window()
+        self.close_window()
 
-    def Close_window(self):
+    def close_window(self):
         if self.wnd:
             self.wnd.Close()
 
-    def OnMyEvent(self, event):
-        # if not self.wnd:
-        self.new_frame()
-
-    def on_translit(self, event):
-        # print(f"on_translit_event called")
-        # time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
+    def get_data_from_clipboard(self):
+        pyperclip.copy(EMPTY_NAME)
+        time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
         keyboard.press_and_release('ctrl+c')
         time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
-        # # keyboard.press_and_release('del')
-        # time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
-        #
-        # win32clipboard.OpenClipboard()
-        # data = win32clipboard.GetClipboardData()
-        # # print(f"{data = }")
-        # win32clipboard.CloseClipboard()
         data_from_clipboard = pyperclip.paste()
-        #
-        letters_rus = "фисвуапршолдьтщзйкыегмцчняФИСDEÀÏРШJËДЬТЩЗЙКЫЕГМЦЧНЯ"
-        letters_eng = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        #
+        if data_from_clipboard == EMPTY_NAME:
+            keyboard.press_and_release('ctrl+c')
+            time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS * 3)
+        data_from_clipboard = pyperclip.paste()
+        return data_from_clipboard
+
+    def do_translit(self, event):
+        data_from_clipboard = self.get_data_from_clipboard()
+
         rus = 1
         for i in data_from_clipboard:
-            if i in letters_rus:
+            if i in LETTERS_RUS:
                 rus += 1
             else:
                 rus -= 1
 
         if rus > 0:
-            mytable = data_from_clipboard.maketrans(letters_rus, letters_eng)
+            mytable = data_from_clipboard.maketrans(LETTERS_RUS, LETTERS_ENG)
         else:
-            mytable = data_from_clipboard.maketrans(letters_eng, letters_rus)
-        # print(f"{mytable = }")
+            mytable = data_from_clipboard.maketrans(LETTERS_ENG, LETTERS_RUS)
+
         try:
             translit_data = data_from_clipboard.translate(mytable)
-            # print(f"{translit_data = }")
-            # print(f"{translit_data.encode('UTF-8') = }")
-            # print(f"{translit_data.encode('ANSI') = }")
-            # print(f"{translit_data.encode('windows-1251') = }")
 
-            # win32clipboard.OpenClipboard()
-            # win32clipboard.EmptyClipboard()
-            # win32clipboard.SetClipboardText(translit_data)
-            # # data = win32clipboard.GetClipboardData()
-            # win32clipboard.CloseClipboard()
             time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
             pyperclip.copy(translit_data)
             time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
-
-            # win32clipboard.OpenClipboard()
-            # print(f"{data = }")
-            # win32clipboard.CloseClipboard()
 
             print(f"{translit_data = }")
 
             keyboard.press_and_release('ctrl+v')
         except UnicodeEncodeError:
             print(f"UnicodeEncodeError...")
-            pass
+            # pass
 
-    def new_frame(self):
+    def new_frame(self, event):
         mouse_pos = win32api.GetCursorPos()
         message = self.do_translate()
+
         self.wnd = PopupWindow(self, mouse_pos, message=message)
         self.wnd.Show()
-        self.timer.Start(10000)
+
+        time_till_pop = 3000 + len(message[10:]) * 70
+        self.timer.Start(time_till_pop)
         # wx.CallLater(10000, self.Close_window)
 
-    def on_key_pressed(self, event):
-        # print("Key pressed")
-        self.popup_window.Close()
-        self.Close()
-        self.task_bar_icon.Destroy()
-
     def do_translate(self):
-        # mouse_pos = win32api.GetCursorPos()
-        empty_name = "<<empty>>"
-        # pyperclip.copy(empty_name)
-        time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
-        keyboard.press_and_release('ctrl+c')
-        pyautogui.hotkey('ctrl', 'c')
-        time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
-        data_from_clipboard = pyperclip.paste()
-        if data_from_clipboard != empty_name:
+        data_from_clipboard = self.get_data_from_clipboard()
 
-            translated_text = db.check_word(data_from_clipboard)
-            # print(f"{translated_text = }")
-            if not translated_text:
-                letters_rus = "фисвуапршолдьтщзйкыегмцчняФИСDEÀÏРШJËДЬТЩЗЙКЫЕГМЦЧНЯ"
-                # letters_eng = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                rus = 0
-                for i in data_from_clipboard:
-                    if i in letters_rus:
-                        rus += 1
-                    else:
-                        rus -= 1
-                if '.' in data_from_clipboard:
-                    data_from_clipboard = data_from_clipboard.replace('.', '. ')
-                if '_' in data_from_clipboard:
-                    data_from_clipboard = data_from_clipboard.replace('_', '-')
-                if rus > 0:
-                    translated_data = make_google_translation(data_from_clipboard, 'en', 'ru')
-                else:
-                    translated_data = make_google_translation(data_from_clipboard, 'ru', 'en')
-                # print(f"{translated_data = }")
-
-                translated_text = translated_data.get('translatedText')
-                # translated_text = data_from_clipboard
-                db.query_save(data_from_clipboard, translated_text)
-                print("new translation")
-            else:
-                print("used existing word from db")
-
-            pyperclip.copy(translated_text)
-            time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
+        if data_from_clipboard == last_word['word']:
+            translated_text = last_word['translation']
         else:
-            translated_text = empty_name
+            if data_from_clipboard != EMPTY_NAME:
+                translated_text = db.check_word(data_from_clipboard)
+                # print(f"{translated_text = }")
+                if not translated_text:
+                    rus = 0
+                    for i in data_from_clipboard:
+                        if i in LETTERS_RUS:
+                            rus += 1
+                        else:
+                            rus -= 1
+                    if '.' in data_from_clipboard:
+                        data_from_clipboard = data_from_clipboard.replace('.', '. ')
+                    if '_' in data_from_clipboard:
+                        data_from_clipboard = data_from_clipboard.replace('_', '-')
+
+                    if rus > 0:
+                        target_language = 'en'
+                        source_language = 'ru'
+                    else:
+                        target_language = 'ru'
+                        source_language = 'en'
+
+                    response = requests.get(
+                        API_URL,
+                        params={"word": data_from_clipboard,
+                                'target_language': target_language,
+                                'source_language': source_language,
+                                },
+                        timeout=5
+                    )
+                    # response = requests.get(
+                    #     API_URL,
+                    #     params={"word": data_from_clipboard,
+                    #             'target_language': 'fh',
+                    #             'source_language': 'hh1',
+                    #             },
+                    #     timeout=5
+                    # )
+                    if response.status_code == 200:
+                        translated_text = response.json()['translation']
+                        print(f"{response.json()['translation'] = }")
+                        # print(f"{response.content = }")
+                        print(f"{response.status_code = }")
+
+                        # save only 1-2 word phrases
+                        if len(data_from_clipboard.strip().split(' ')) <= 2:
+                            db.query_save(data_from_clipboard, translated_text)
+                    else:
+                        translated_text = response.json()['error']
+
+                    print("new translation")
+                else:
+                    print("used existing word from db")
+                # print(f"{translated_text = }")
+                pyperclip.copy(translated_text)
+                time.sleep(TIME_SLEEP_BETWEEN_KEYPRESS)
+            else:
+                translated_text = EMPTY_NAME
+        last_word['word'] = data_from_clipboard
+        last_word['translation'] = translated_text
+
         print(f"{translated_text = }")
         # keyboard.press_and_release('ctrl+v')
-        mouse_pos = win32api.GetCursorPos()
         return translated_text
 
 
@@ -280,34 +281,31 @@ class WorkerThread(threading.Thread):
     def __init__(self, window):
         threading.Thread.__init__(self)
         self.window = window
-        keyboard.add_hotkey('ctrl+shift+1', self.do_test)
+        self.window.wnd = None
+        keyboard.add_hotkey('ctrl+shift+1', self.do_translate)
         keyboard.add_hotkey('ctrl+shift+`', self.do_translit)
 
-    def do_test(self):
-        # print("Testing")
+    def do_translate(self):
+        if self.window.wnd:
+            self.window.wnd.Close()
         evt = wx.IconizeEvent(id=10, iconized=True)
         wx.PostEvent(self.window.GetEventHandler(), evt)
 
     def do_translit(self):
-        # print("Testing translit")
         evt = wx.IconizeEvent(id=20, iconized=True)
         wx.PostEvent(self.window.GetEventHandler(), evt)
-
-    # def run(self):
-    #     keyboard.wait('esc')
 
 
 class App(wx.App):
     def __init__(self, redirect):
-        # self.db = db
         super().__init__(redirect=redirect)
+        self.frame = None
 
     def OnInit(self):
         self.frame = MainWindow(None, "Focus mode")
         # wx.lib.inspection.InspectionTool().Show()  # to inspect all parameters of windows\panels\widgets
         TaskBarIcon(self.frame)
         return True
-
 
 
 app = App(redirect=False)
